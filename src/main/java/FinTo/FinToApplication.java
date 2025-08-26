82dc4280a0e393f5664b07ae973d511d793ff22a
Todom2 <schoolpang@naver.com>
2025-08-26 16:54:13 +0900

    프로젝트 기본 설정

---
+++ src/main/java/FinTo/FinToApplication.java
@@ -0,0 +1,13 @@
+package FinTo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinToApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinToApplication.class, args);
+	}
+
+}
