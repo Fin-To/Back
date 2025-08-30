0819bbfe742ac14e82fea7f716a8bd5a97195f1c
Gyehyun Park <gyehyunbak@gmail.com>
2025-08-31 00:10:18 +0900

    feature: SecurityConfig, SecurityTestController 추가

---
+++ src/test/java/FinTo/global/security/SecurityTestControllerTest.java
@@ -0,0 +1,16 @@
+package FinTo.global.security;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SecurityTestControllerTest {
+
+    @Test
+    void anonymous() {
+    }
+
+    @Test
+    void authenticated() {
+    }
+}
