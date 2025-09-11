db0494a04f95f9348fe614b5e510debeb19ada49
Todom2 <schoolpang@naver.com>
2025-09-11 16:29:30 +0900

    style: /api URL 경로 삭제

--- src/main/java/FinTo/domain/mentoring/controller/MentoringController.java
+++ src/main/java/FinTo/domain/mentoring/controller/MentoringController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/mentoring")
+@RequestMapping("/mentoring")
 @RequiredArgsConstructor
 public class MentoringController {
 
--- src/main/java/FinTo/domain/review/controller/ReviewController.java
+++ src/main/java/FinTo/domain/review/controller/ReviewController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/reviews")
+@RequestMapping("/reviews")
 @RequiredArgsConstructor
 public class ReviewController {
     private final ReviewService reviewService;
