202ec62c880d4fdbbd1153cf9d4c54af0ca1ef4b
KJK1023 <102352312+KJK1023@users.noreply.github.com>
2025-09-11 20:47:48 +0900

    feature: 요일 및 시간 반환 기능 구현 (#39)

--- src/main/java/FinTo/domain/mentoring/controller/MentoringController.java
+++ src/main/java/FinTo/domain/mentoring/controller/MentoringController.java
@@ -3,7 +3,9 @@ package FinTo.domain.mentoring.controller;
 import FinTo.domain.mentoring.dto.request.MentoringCreateRequestDto;
 import FinTo.domain.mentoring.dto.request.MentoringUpdateRequestDto;
 import FinTo.domain.mentoring.dto.response.MentoringCardResponseDto;
+import FinTo.domain.mentoring.dto.response.MentoringDayResponseDto;
 import FinTo.domain.mentoring.dto.response.MentoringMyListResponseDto;
+import FinTo.domain.mentoring.dto.response.MentoringTimeResponseDto;
 import FinTo.domain.mentoring.service.MentoringService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -12,8 +14,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/mentoring")
+@RequestMapping("/mentorings")
 @RequiredArgsConstructor
 public class MentoringController {
 
@@ -25,7 +29,7 @@ public class MentoringController {
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/my")
+    @GetMapping("/me")
     public ResponseEntity<Page<MentoringMyListResponseDto>> getMyMentorings(
             @RequestParam Long mentorId,
             @RequestParam(defaultValue = "0") int page,
@@ -53,5 +57,16 @@ public class MentoringController {
         return ResponseEntity.ok().build();
     }
 
+    @GetMapping("/{mentoringId}/days")
+    public ResponseEntity<List<MentoringDayResponseDto>> getDays(@PathVariable Long mentoringId) {
+        return ResponseEntity.ok(mentoringService.getMentoringDays(mentoringId));
+    }
 
+    @GetMapping("/{mentoringId}/times")
+    public ResponseEntity<List<MentoringTimeResponseDto>> getTimes(
+            @PathVariable Long mentoringId,
+            @RequestParam String day
+    ) {
+        return ResponseEntity.ok(mentoringService.getMentoringTimes(mentoringId, day));
+    }
 }
