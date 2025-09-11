08af06c7092b056aa77dd3928d4834094b78fd90
Gyehyun Park <gyehyunbak@gmail.com>
2025-09-11 16:13:25 +0900

    feature: 언어 생성 조회 구현

--- src/main/java/FinTo/domain/language/domain/Language.java
+++ src/main/java/FinTo/domain/language/domain/Language.java
@@ -2,9 +2,11 @@ package FinTo.domain.language.domain;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 public class Language {
     @Id
     @GeneratedValue
--- src/main/java/FinTo/domain/language/service/LanguageServiceImpl.java
+++ src/main/java/FinTo/domain/language/service/LanguageServiceImpl.java
@@ -1,9 +1,15 @@
 package FinTo.domain.language.service;
 
+import FinTo.domain.language.domain.Language;
+import FinTo.domain.language.dto.LanguageCreateRequestDto;
+import FinTo.domain.language.dto.LanguageResponseDto;
+import FinTo.domain.language.dto.LanguagesResponseDto;
 import FinTo.domain.language.repository.LanguageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,4 +17,21 @@ import org.springframework.transaction.annotation.Transactional;
 public class LanguageServiceImpl implements LanguageService {
 
     private final LanguageRepository languageRepository;
+
+    @Override
+    public void create(LanguageCreateRequestDto requestDto) {
+        Language language = new Language();
+        language.setName(requestDto.getName());
+        languageRepository.save(language);
+    }
+
+    @Override
+    public LanguagesResponseDto getAll() {
+        List<LanguageResponseDto> languageDtos = languageRepository.findAllByOrderByNameAsc()
+                .stream()
+                .map(l -> new LanguageResponseDto(l.getId(), l.getName()))
+                .toList();
+
+        return new LanguagesResponseDto(languageDtos);
+    }
 }
